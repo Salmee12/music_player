5ed4db556832f9ba490ef7161e348510ca936ae8
saihamrafi37 <saihamrafii@gmail.com>
2025-08-13 23:12:18 +0600

    Update main.dart

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/core/theme/theme.dart';
+
+import 'features/auth/view/pages/signup_page.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme:AppTheme.darkThemeMode,
+      home: const SignupPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
