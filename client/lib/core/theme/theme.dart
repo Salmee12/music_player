a1e06e4980ee4161cee55a0738b7747ac085c1f4
saihamrafi37 <saihamrafii@gmail.com>
2025-08-13 23:09:30 +0600

    Update theme.dart

--- client/lib/core/theme/theme.dart
+++ client/lib/core/theme/theme.dart
@@ -1,1 +1,23 @@
+import 'app_pallete.dart';
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static OutlineInputBorder _border(Color color) => OutlineInputBorder(
+    borderSide: BorderSide(
+      color: color,
+      width: 3,
+    ),
+    borderRadius: BorderRadius.circular(10),
+  );
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: Pallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      enabledBorder: _border(Pallete.borderColor),
+      focusedBorder: _border(Colors.blue),
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: Pallete.backgroundColor,
+    ),
+  );
+}
