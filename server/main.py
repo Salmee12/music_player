b45a8c4f0620e2e5cb70de5c49971d02cf375ccb
Saad Salmee <salmeeechy@gmail.com>
2025-09-05 23:50:05 +0600

    song upload method created

--- server/main.py
+++ server/main.py
@@ -1,14 +1,16 @@
 
 from fastapi import FastAPI
-from routes import auth#, song
+from routes import auth, song
 from models.base import Base
 from database import engine
 from fastapi.middleware.cors import CORSMiddleware
 
+
+
 app = FastAPI()
 
 app.include_router(auth.router, prefix='/auth')
-#app.include_router(song.router, prefix='/song')
+app.include_router(song.router, prefix='/song')
 
 Base.metadata.create_all(engine)
 
